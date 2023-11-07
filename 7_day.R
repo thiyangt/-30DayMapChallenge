f5174336f25d3b27f26f2d60ea237b9ab7812d9b
thiyangt <thiyanga.talagala@monash.edu>
2023-11-07 23:43:10 +0530

    updated

--- 7_day.R
+++ 7_day.R
@@ -1,15 +1,11 @@
 
-
-library(tidygeocoder)
-df <- tidygeocoder::geocode(df, address = addr, method = "osm")
-library(ggmap)
 #https://springernature.figshare.com/articles/dataset/Hospital_IDs_names_and_coordinates_csv_/8319737
 # subset the data
 library(tidyverse)
 location <- read_csv(here::here("myhospitals-contact-details.csv"))
 head(location)
 location <- location |> 
-  filter(Suburb == "Melbourne")
+  filter(Suburb == "Cairns")
 
 library(mapview)
 library(sf)
