cff9973c9e5e7f17e36387fb937ce9559decdadd
thiyangt <thiyanga.talagala@monash.edu>
2023-11-09 21:24:13 +0530

    updated

--- 9_day.R
+++ 9_day.R
@@ -8,6 +8,7 @@ library(here)
 library(sf)
 library(rgdal)
 library(rgeos)
+library(sp)
 #install.packages("remotes")
 #remotes::install_github("ropensci/geojsonio")
 
@@ -21,4 +22,21 @@ View(trees_usa)
 spdf <- readOGR("us_states_hexgrid.geojson")
 spdf@data <- spdf@data |>
   mutate(google_name = gsub(" \\(United States\\)", "", google_name))
-head(spdf@data)
+
+centers <- cbind.data.frame(data.frame(gCentroid(spdf, byid=TRUE), id=spdf@data$iso3166_2))
+
+spdf_fortified <- tidy(spdf)
+View(spdf_fortified)
+
+spdf_trees <- left_join(spdf@data, trees_usa)
+View(spdf_trees)
+
+# Show it
+ggplot() +
+  geom_polygon(data = spdf_fortified, aes( x = long, y = lat, group = group),  color="white") +
+  geom_text(data=centers.new, aes(x=x, y=y, label=id)) +
+  theme_void() +
+  coord_map() +
+  labs(x= 'Longitute' , y = 'Latitude',
+       title = "Percentage of Forest Acres by States in the US",
+       caption = 'Source: https://www.gotreequotes.com/states-with-most-forested-acres/#US_Forest_Cover_Map\n#30DayMapChallenge\nThiyanga S. Talagala')
